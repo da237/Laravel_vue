a518722c791632025ab83b918c583e67528e47c4
da237 <daniel951931@outlook.es>
2025-07-23 14:52:59 -0500

    Feat:Se realiza la creacion de la tabla, modelo y controlador

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\VisitController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('visits', VisitController::class);
+});
